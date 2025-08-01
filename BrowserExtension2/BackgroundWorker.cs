efa120375f9e503d889acfa8c475801db9993262
Stef Heyenrath <stef.heyenrath@gmail.com>
2025-08-01 20:46:36 +0200

    BrowserExtension2

---
+++ BrowserExtension2/BackgroundWorker.cs
@@ -0,0 +1,58 @@
+﻿using Blazor.BrowserExtension;
+using WebExtensions.Net.Menus;
+using WebExtensions.Net.Tabs;
+using MenuCreateProperties = WebExtensions.Net.Menus.CreateProperties;
+
+namespace BrowserExtension2;
+
+public partial class BackgroundWorker : BackgroundWorkerBase
+{
+    private static readonly MenuCreateProperties CopyText = new()
+    {
+        Id = nameof(CopyText),
+        Title = "Copy",
+        Contexts = [ContextType.Selection]
+    };
+
+    [BackgroundWorkerMain]
+    public override void Main()
+    {
+        WebExtensions.Runtime.OnInstalled.AddListener(OnInstalled);
+    }
+
+    async Task OnInstalled()
+    {
+        var indexPageUrl = WebExtensions.Runtime.GetURL("index.html");
+        await WebExtensions.Tabs.Create(new()
+        {
+            Url = indexPageUrl
+        });
+
+        WebExtensions.ContextMenus.Create(CopyText);
+
+        WebExtensions.ContextMenus.OnClicked.AddListener(OnContextMenuClicked);
+    }
+
+    private async void OnContextMenuClicked(OnClickData onClickData, Tab tab)
+    {
+        if (!tab.WindowId.HasValue)
+        {
+            return;
+        }
+
+        if (onClickData.MenuItemId.Value as string == CopyText.Id && !string.IsNullOrWhiteSpace(onClickData.SelectionText))
+        {
+            await CreateNewTabAsync(onClickData.SelectionText);
+        }
+    }
+
+    private async Task CreateNewTabAsync(string selectionText)
+    {
+        _ = await WebExtensions.Tabs.Create(new()
+        {
+            Url = "https://mingyaulee.github.io/Blazor.BrowserExtension"
+        });
+
+        Console.WriteLine(selectionText);
+    }
+}
---
+++ BrowserExtension2/Program.cs
@@ -0,0 +1,27 @@
+﻿using Blazor.BrowserExtension;
+using BrowserExtension2;
+using BrowserExtension2.Pages;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.UseBrowserExtension(browserExtension =>
+{
+    if (browserExtension.Mode == BrowserExtensionMode.ContentScript)
+    {
+        builder.RootComponents.Add<ContentScript>("#My_Unique_Extension_App_Id");
+    } 
+    else if (browserExtension.Mode == BrowserExtensionMode.Background)
+    {
+        builder.RootComponents.AddBackgroundWorker<BackgroundWorker>();
+    }
+    else
+    {
+        builder.RootComponents.Add<App>("#app");
+        builder.RootComponents.Add<HeadOutlet>("head::after");
+    }
+});
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+await builder.Build().RunAsync();
