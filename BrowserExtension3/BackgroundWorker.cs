fd2d14e4adbcefcd21550d9d49adcef613462087
Stef Heyenrath <stef.heyenrath@gmail.com>
2025-08-01 20:56:58 +0200

    BrowserExtension3

---
+++ BrowserExtension3/BackgroundWorker.cs
@@ -0,0 +1,63 @@
+﻿using Blazor.BrowserExtension;
+using WebExtensions.Net.Menus;
+using WebExtensions.Net.Tabs;
+using MenuCreateProperties = WebExtensions.Net.Menus.CreateProperties;
+
+namespace BrowserExtension3;
+
+public partial class BackgroundWorker : BackgroundWorkerBase
+{
+    private static readonly MenuCreateProperties Extension3 = new()
+    {
+        Id = nameof(Extension3),
+        Title = nameof(Extension3),
+        Contexts = [ContextType.Selection]
+    };
+
+    [BackgroundWorkerMain]
+    public override void Main()
+    {
+        WebExtensions.Runtime.OnInstalled.AddListener(OnInstalled);
+
+        WebExtensions.Runtime.OnStartup.AddListener(OnStartup);
+    }
+
+    async Task OnInstalled()
+    {
+        var indexPageUrl = WebExtensions.Runtime.GetURL("index.html");
+        await WebExtensions.Tabs.Create(new()
+        {
+            Url = indexPageUrl
+        });
+    }
+
+    void OnStartup()
+    {
+        WebExtensions.ContextMenus.Create(Extension3);
+
+        WebExtensions.ContextMenus.OnClicked.AddListener(OnContextMenuClicked);
+    }
+
+    private async void OnContextMenuClicked(OnClickData onClickData, Tab tab)
+    {
+        if (!tab.WindowId.HasValue)
+        {
+            return;
+        }
+
+        if (onClickData.MenuItemId.Value as string == Extension3.Id && !string.IsNullOrWhiteSpace(onClickData.SelectionText))
+        {
+            await CreateNewTabAsync(onClickData.SelectionText);
+        }
+    }
+
+    private async Task CreateNewTabAsync(string selectionText)
+    {
+        _ = await WebExtensions.Tabs.Create(new()
+        {
+            Url = "https://google.com"
+        });
+
+        Console.WriteLine(selectionText);
+    }
+}
---
+++ BrowserExtension3/wwwroot/app.js
@@ -0,0 +1,13 @@
+﻿/**
+ * Called before Blazor starts.
+ * @param {object} options Blazor WebAssembly start options. Refer to https://github.com/dotnet/aspnetcore/blob/main/src/Components/Web.JS/src/Platform/WebAssemblyStartOptions.ts
+ * @param {object} extensions Extensions added during publishing
+ * @param {object} blazorBrowserExtension Blazor browser extension instance
+ */
+export function beforeStart(options, extensions, blazorBrowserExtension) {
+    if (blazorBrowserExtension.BrowserExtension.Mode === blazorBrowserExtension.Modes.ContentScript) {
+        const appDiv = document.createElement("div");
+        appDiv.id = "My_Unique_Extension_App_Id";
+        document.body.appendChild(appDiv);
+    }
+}
